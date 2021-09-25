993b51ce101eb83f7d4ea5386564384000055d7a
sohammondal81@gmail.com <mondalsoham42@gmail.com>
2021-09-25 20:29:28 +0530

    Updates STATIC_ROOT

--- twet/settings.py
+++ twet/settings.py
@@ -67,7 +67,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
